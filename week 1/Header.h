b90074dacbf44f8043ea63906639f8557c6e1de6
Ivan Velkov <sasmose@yahoo.com>
2016-03-04 14:36:00 +0200

    Fixed Documentation

--- week 1/Header.h
+++ week 1/Header.h
@@ -1,10 +1,10 @@
 #ifndef HEADER
 #define HEADER
 
-
-
 #include <iostream>
 
 
 void print();
-#endif // !HEADER
+
+
+#endif // HEADER
--- week 1/main.cpp
+++ week 1/main.cpp
@@ -1,5 +1,13 @@
+﻿#include <iostream>
+
 #include <iostream>
 
+// замества съдържанието на Header.h
+// като същите неща прави и за стандартните библиотеки
+#include "Header.h" 
+/*
+#ifndef HEADER
+#define HEADER
 
 #include <iostream>
 
@@ -7,6 +15,11 @@
 void print();
 
 
+#endif // HEADER
+*/
+
+#include "Const.h" // замества съдържанието на Const.h
+/*
 #ifndef CONST_H
 #define CONST_H
 
@@ -14,19 +27,27 @@ const int abc = 42;
 
 
 #endif 
+*/
 
+
+#include "Const.h" // замества съдържанието на Const.h
+/*
 #ifndef CONST_H
 #define CONST_H
 
 const int abc = 42;
 
 
-#endif 
+#endif
+*/
 
+// замества всяко срещане на PI с 3.14159
+#define PI 3.14159 
 
-
+// изчислява квадрат
 #define square(n) (n)*(n)
 
+// грешен квадрат
 #define square_wrong(n) \
 					n*n
 
@@ -38,9 +59,11 @@ const int abc = 42;
 				else\
 					std::cout<< n<< std::endl;
 
-
+// превръща подадения аргумент в стринг
 #define toStr(exp) #exp
 
+// печата подадения аргумент и казва дали 
+// е истина или лъжа
 #define checker(exp)\
 				if(exp)\
 				{\
@@ -50,6 +73,15 @@ const int abc = 42;
 					std::cout<< "not true:"<< #exp<< std::endl;
 
 
+// обратната наклонена черта '\' \
+указва, че даденият израз продължава \
+и на следващия ред , като освен за макроси \
+може да бъде използвано за коментари, стрингове \
+и т.н. ,но ако бъде използвано за "//" коментари \
+компилаторът ще пищи, но все пак кодът ще се компилира
+
+
+// слепя подадените аргументи
 #define concat(x, y) x##y
 
 
@@ -58,16 +90,18 @@ int main()
 
 
 
-	std::cout << __FILE__ << std::endl;
-	std::cout << __LINE__ << std::endl;
+	std::cout << __FILE__ << std::endl; // печата името на файла
+	std::cout << __LINE__ << std::endl; // печета реда, на който се намира
+
+// предефинира __LINE__ и __FILE__
 #line 7 "az sym gotin"
 
 	
-	std::cout << __FILE__ << std::endl;
-	std::cout << __LINE__ << std::endl;
+	std::cout << __FILE__ << std::endl; // печата предефинираното име
+	std::cout << __LINE__ << std::endl; // печата реда спрямо #line
 
-	std::cout << __DATE__ << std::endl;
-	std::cout << __TIME__ << std::endl;
+	std::cout << __DATE__ << std::endl; // печата датата
+	std::cout << __TIME__ << std::endl; // печата часа 
 
 	min(3, 4);
 
@@ -86,13 +120,28 @@ int main()
 
 	std::cout << ab << std::endl;
 
+	// същото като 
+	//std::cout << ab << std::endl;
 	std::cout << concat(a,b) << std::endl;
 
-	std::cout << concat(fo, o) << std::endl;
+	// същото като 
+	//std::cout << foo << std::endl;
+	std::cout << concat(fo, o) << std::endl; // 
 
+	// същото като 
+	// float chislo;
+	concat(flo, at) chislo; // дефинираме float с име chislo и работи Kappa
+	chislo = 32.2f;
 
+	std::cout << chislo << std::endl;
 
 	print();
 
 	return 0;
 }
+
+// ако долното бъде разкоментирано 
+// и бъдат изтрити ; и } програмата 
+// все пак ще се компилира 
+
+//#include "endfile.h"
